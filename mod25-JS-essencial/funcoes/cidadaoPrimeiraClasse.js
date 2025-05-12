2300bcfc885305241fe83bf5c4b6f0ba7a0be931
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-12 14:48:44 -0300

    feat: diversos modos de declarar e usar funções

---
+++ mod25-JS-essencial/funcoes/cidadaoPrimeiraClasse.js
@@ -0,0 +1,31 @@
+
+//criando função de forma literal
+function fun1(){
+}
+
+//armazenando função em variável
+const fun2 = function() {
+
+}
+
+//armazenar função em array
+const array = [function(a, b) {return a + b}, fun1, fun2]
+
+//função armazenada em atributo de objeto
+const obj = {}
+onbeforetoggle.falar = function () { return "Eai"}
+
+//passar função como parametro
+function run(fun){
+    fun()
+}
+
+run(function(){console.log("Executando...")})
+
+
+//função pode retornar/conter outra função
+function soma(a,b){
+    return function(c){
+        console.log(a + b + c)
+    }
+}
